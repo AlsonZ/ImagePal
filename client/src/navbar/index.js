7dcb0990c6367998bb857fbb2fbef02f8b199c7d
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-06-25 05:47:31 +1000

    start navbar

---
+++ client/src/navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+const Nav = () => {
+
+  return (
+    <nav>
+      <ul>
+        <li>ImagePal</li>
+        <li>LeaderBoard</li>
+        <li>Sign In</li>
+        <li>Sign Up</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
