e8927c725b43713b696364c9c8c79772c613e406
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-06-29 08:28:50 +1000

    add uploadImage function to cloudinary for newpost component

--- client/src/NewPostComponent/index.js
+++ client/src/NewPostComponent/index.js
@@ -1,12 +1,31 @@
 import React, { useState } from 'react';
 import './style.css';
+import { setRandomFallback } from 'bcryptjs';
 
 const NewPost = (props) => {
 
   const [fileName, setFileName] = useState("Choose File");
+  const [file, setFile] = useState('');
+  const [image, setImage] = useState('');
 
   const showName = (e) => {
     setFileName(e.target.files[0].name);
+    setFile(e.target.files);
+  }
+  const uploadImage = async (e) => {
+    e.preventDefault();
+    console.log(file[0].name);
+    let data = new FormData();
+    data.append('file', file[0]);
+    data.append('upload_preset', 'imagepal');
+    const url = '';//check .env file for url
+    const res = await fetch(url, {
+      method: 'POST',
+      body: data
+    })
+    const resData = await res.json();
+    setImage(resData.secure_url);
+
   }
 
   return (
@@ -20,8 +39,11 @@ const NewPost = (props) => {
             <span className="browse">Browse</span>
             <input type="file" className="file-input" onChange={showName}/>
           </label>
-          <input type="submit" className="button" value="Upload"/>
+          <input type="submit" className="button" value="Upload" onClick={uploadImage}/>
         </form>
+        <div>
+          <img src={image}/>
+        </div>
       </div>
     </div>
   );
