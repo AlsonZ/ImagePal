56726dcbbfddc04ee0866f1e4d5be1ed77c34cd4
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-07-29 22:48:55 +1000

    test filepath change for tmp filepath

--- routes/posts.js
+++ routes/posts.js
@@ -153,7 +153,7 @@ router.post('/newComment/:postID', checkLoggedIn, async (req, res) => {
 })
 
 const handleImage = async (file, fileName, req) => {
-  const filepath = `./tmp/${fileName}`;
+  const filepath = `/tmp/${fileName}`;
   file.mv(filepath, (error) => {
     if(error) {
       console.log('handelImageMV ', error);
